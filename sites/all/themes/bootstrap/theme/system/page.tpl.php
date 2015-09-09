c25c31493df114958f45dd16f529b3abe7f53b7a
ramon <ramonltorres@gmail.com>
2015-09-09 16:27:14 -0400

    secondary navbar

--- sites/all/themes/bootstrap/theme/system/page.tpl.php
+++ sites/all/themes/bootstrap/theme/system/page.tpl.php
@@ -73,45 +73,40 @@
  * @ingroup themeable
  */
 ?>
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="container">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-      <?php endif; ?>
-
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
-
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
+<div class="navBar">
+  <div class="row">
+    <a href="<?php print $GLOBALS['base_path'];?>"><div class="navBarLogo"></div></a>
+    <div class="col-md-6 pull-right p-0">
+      <ul class="nav nav-pills">
+	<li class="dropdown">
+          <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">EVENTS <span class="caret"></span></a>
+          <ul class="dropdown-menu" aria-labelledby="drop2">
+            <li>
+              <a href="<?php print $GLOBALS['base_path'];?>events/upcoming">Upcoming <i class="zmdi zmdi-calendar-note pull-right"></i></a>
+            </li>
+            <li>
+              <a href="<?php print $GLOBALS['base_path'];?>events/recaps">Recaps <i class="zmdi zmdi-tv-play pull-right"></i></a>
+            </li>
+          </ul>
+        </li>
+	<li class="dropdown">
+          <a href="<?php print $GLOBALS['base_path'];?>education" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">EDUCATION <span class="caret"></span></a>
+          <ul class="dropdown-menu" aria-labelledby="drop2">
+            <li>
+              <a href="<?php print $GLOBALS['base_path'];?>education/interviews">Interviews <i class="zmdi zmdi-camera-mic pull-right"></i></a>
+            </li>
+            <li>
+              <a href="<?php print $GLOBALS['base_path'];?>education/articles">Articles <i class="zmdi zmdi-view-headline pull-right"></i></a>
+            </li>
+          </ul>
+        </li>
+	<li><a href="<?php print $GLOBALS['base_path'];?>community">COMMUNITY</a></li>
+	<li><a href="<?php print $GLOBALS['base_path'];?>blog">BLOG</a></li>
+      </ul>
     </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
-    <?php endif; ?>
   </div>
-</header>
+</div>
+<div class="navBarBoxShadow col-md-12"></div>
 
 <div class="main-container container">
 
