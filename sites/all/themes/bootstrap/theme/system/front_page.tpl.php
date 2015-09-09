eb7c1ef4509b66fc71b63f28e864e4e31b94303c
ramon <ramonltorres@gmail.com>
2015-09-09 17:38:09 -0400

    auto centralize height

--- sites/all/themes/bootstrap/theme/system/front_page.tpl.php
+++ sites/all/themes/bootstrap/theme/system/front_page.tpl.php
@@ -34,75 +34,84 @@
 <div class="navBarBoxShadow col-md-12 landing"></div>
 <div class="body">
   <div class="col-md-12 anchor land">
-     <video autoplay poster="" loop id="bgvid" preload="auto">
-   <source src="<?php print $GLOBALS['base_path'];?>sites/default/files/movie.mp4" type="video/mp4"> 
-     <source src="<?php print $GLOBALS['base_path'];?>sites/default/files/movie.webm" type="video/webm">
-     </video>
-     <div class="landingLogo"></div>
+    <video autoplay poster="" loop id="bgvid" preload="auto">
+      <source src="<?php print $GLOBALS['base_path'];?>sites/default/files/movie.mp4" type="video/mp4"> 
+	<source src="<?php print $GLOBALS['base_path'];?>sites/default/files/movie.webm" type="video/webm">
+    </video>
+    <div class="landingLogo"></div>
   </div>
   <div class="col-md-12 anchor about">
-     <div class="row col-md-8">
-       <div class="row" style="margin-top: 10%;">
-         <img class="logo_apple" src="<?php print $GLOBALS['base_path'];?>sites/default/files/highny_logoapple_01.png"/><div class="pull-right col-md-9">
-	   <h3>HIGH NY IS A COMMUNITY</h3>
-	   <p>Dicated to improving the way society interacts with Cannabis. <a href="#signup">Join our mailing list</a></p>
-	 </div>
-       </div>
-     </div>
-     <div class="row col-md-8 text">
-     <div class="col-md-5">We host New York's largest Cannabis Meetup</div><div class="col-md-2"><a href="http://www.meetup.com/HighNY/" target="_blank"><img class="pull-left meetup" src="<?php print $GLOBALS['base_path'];?>sites/default/files/meetup.png" width="150"/></a></div><div class="col-md-5">Support the momement by joining the Meetup!</div>
-     </div>
+    <div class="autoHeight">
+      <div class="row col-md-8">
+	<div class="row">
+          <img class="logo_apple" src="<?php print $GLOBALS['base_path'];?>sites/default/files/highny_logoapple_01.png"/><div class="pull-right col-md-9">
+	    <h3>HIGH NY IS A COMMUNITY</h3>
+	    <p>Dicated to improving the way society interacts with Cannabis. <a href="#signup">Join our mailing list</a></p>
+	  </div>
+	</div>
+      </div>
+      <div class="row col-md-8 text">
+	<div class="col-md-5">We host New York's largest Cannabis Meetup</div><div class="col-md-2"><a href="http://www.meetup.com/HighNY/" target="_blank"><img class="pull-left meetup" src="<?php print $GLOBALS['base_path'];?>sites/default/files/meetup.png" width="150"/></a></div><div class="col-md-5">Support the momement by joining the Meetup!</div>
+      </div>
+    </div>
   </div>
 
   <div class="col-md-12 anchor roll">
-     <div class="row">
-       <h3>Cannabis Stats</h3>
-     </div>
-     <div class="row col-md-10">
-       <div class="col-md-6">
-         <div class="stats">
-         NEW YORKERS WHO SUPPORT MEDICAL USE
-         <div class="statsContainer 88" stats="88"><div class="bar"></div></div>
-         </div>
-         <div class="stats">
-         NYC POSSESSION ARRESTS THAT ARE OF BLACKS & HISPANICS
-         <div stats="86" class="statsContainer 86"><div class="bar"></div></div>
-         </div>
-         <div class="stats">
-         INCREASE IN NYC MARIJUANA ARRESTS: 1,000% IN THE LAST 25 YEARS
-         <div stats="1000" class="statsContainer 1000" ><div class="bar"></div></div>
-         </div>
-       </div>
-       <div class="col-md-6">
-         <div class="stats">
-         NEW YORKERS WHO SUPPORT FULL LEGALIZATION OF CANNABIS
-         <div stats="57" class="statsContainer 57" ><div class="bar"></div></div>
-         </div>
-         <div class="stats">
-         INCREASE IN DEATHS FROM ACCIDENTAL OVERDOSE ON PRESCRIPTION DRUG: 360% IN U.S. SINCE 1999
-         <div stats="360" class="statsContainer 360"><div class="bar"></div></div>
-         </div>
-       </div>
-     </div>
+    <div class="autoHeight">
+      <div class="row">
+	<h3>Cannabis Stats</h3>
+      </div>
+      <div class="row col-md-10">
+	<div class="col-md-6">
+          <div class="stats">
+            NEW YORKERS WHO SUPPORT MEDICAL USE
+            <div class="statsContainer 88" stats="88"><div class="bar"></div></div>
+          </div>
+          <div class="stats">
+            NYC POSSESSION ARRESTS THAT ARE OF BLACKS & HISPANICS
+            <div stats="86" class="statsContainer 86"><div class="bar"></div></div>
+          </div>
+          <div class="stats">
+            INCREASE IN NYC MARIJUANA ARRESTS: 1,000% IN THE LAST 25 YEARS
+            <div stats="1000" class="statsContainer 1000" ><div class="bar"></div></div>
+          </div>
+	</div>
+	<div class="col-md-6">
+          <div class="stats">
+            NEW YORKERS WHO SUPPORT FULL LEGALIZATION OF CANNABIS
+            <div stats="57" class="statsContainer 57" ><div class="bar"></div></div>
+          </div>
+          <div class="stats">
+            INCREASE IN DEATHS FROM ACCIDENTAL OVERDOSE ON PRESCRIPTION DRUG: 360% IN U.S. SINCE 1999
+            <div stats="360" class="statsContainer 360"><div class="bar"></div></div>
+          </div>
+	</div>
+      </div>
+    </div>
   </div>
 
   <div class="col-md-12 anchor signup">
-     <a name="signup"></a>
-     <div class="row">
-     <div>
-     <h3>Stay tuned</h3>
-     <div class="col-md-12">
-     <?php 
-     $block = block_load('mailing_list', '1');
-     $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block))));
-     print $output;
-     ?>
-     </div>
-     </div>
-     </div>
+    <div class="autoHeight">
+      <a name="signup"></a>
+      <div class="row">
+	<div>
+	  <h3>Stay tuned</h3>
+	  <div class="col-md-12">
+	    <?php 
+	       $block = block_load('mailing_list', '1');
+	       $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block))));
+	       print $output;
+	    ?>
+	  </div>
+	</div>
+      </div>
+    </div>
   </div>
 
   <div class="col-md-12 anchor partners">
+    <div class="autoHeight">
+
+    </div>
 
   </div>
 </div>
@@ -143,7 +152,13 @@
          var resize = function () {
              jQuery('.anchor').each(function (k,v) {
                  jQuery(v).height(windowHeight());
-                 //jQuery('#bgvid').height(windowHeight());
+                 var div = jQuery(v).find('.autoHeight');
+                 if(div.height()) {
+                   var marginTop = (windowHeight() - div.height()) / 2;
+                   div.css({
+                       'margin-top': marginTop
+                   });
+                 } 
              });
          };
 
